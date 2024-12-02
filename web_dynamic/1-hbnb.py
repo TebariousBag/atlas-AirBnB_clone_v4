e48e027fbec5ac5e312d57fe34c13ce5d20e33ea
“Tristian <tristian.davis@atlasschool.com>
2024-12-02 21:11:12 +0000

    messed something up, trying to fix

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 # ------------------ #
-# changed app route to /1-hbnb/
+# changed app route
 @app.route('/1-hbnb/')
 def hbnb():
     """ HBNB is alive! """
@@ -36,10 +36,10 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    # ------------------ #
-    # added cache_id, and changed to 0-hbnb.html
+# ------------------ #
+# added cache_id, and changed to 1-hbnb.html
     cache_id = uuid.uuid4()
-    return render_template('/1-hbnb/',
+    return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
@@ -47,4 +47,4 @@ def hbnb():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
