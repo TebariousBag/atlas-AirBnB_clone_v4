036f6e605b7e17938dc99bbb88f903c00442c337
“Tristian <tristian.davis@atlasschool.com>
2024-12-02 21:38:56 +0000

    files in place and routes changed for 2-hbnb

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,10 +7,13 @@ from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
 from flasgger import Swagger
 from flasgger.utils import swag_from
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+# ------------------ #
+# changed CORS to allow all origins
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
