baab0d19dfb62fcd0f309dfdab7cb723b4903204
“Tristian <tristian.davis@atlasschool.com>
2024-12-03 10:54:38 +0000

    changed the ports back to 5000, i had changed them since mac doesn't like port 5000

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -50,4 +50,4 @@ def hbnb():
 # turned on debugger
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5002, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
