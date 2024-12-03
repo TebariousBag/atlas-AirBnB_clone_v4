823b51eeb6d761258bab0a5bac2c41305e765c10
“Tristian <tristian.davis@atlasschool.com>
2024-12-03 01:16:24 +0000

    can't get available to turn red

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -2,33 +2,32 @@
 
 // document ready function
 $(document).ready(function() {
-	// empty to keep track of selected
-	const selected = {};
-  
-	// add checkbox
-	$('input[type="checkbox"]').change(function() {
-		// get data for each
-	  const amenityId = $(this).attr('data-id');
-	  const amenityName = $(this).attr('data-name');
+    let selected = {};
+
+		// add checkbox
+    $('input[type="checkbox"]').change(function() {
+        const id = $(this).attr('data-id');
+        const name = $(this).attr('data-name');
 		// if checked add to selected
-	  if ($(this).is(':checked')) {
-		selected[amenityId] = amenityName;
-	  } else {
-		delete selected[amenityId];
-	  }
-	  // join selected
-	  const amenitiesList = Object.values(selected).join(', ');
-	  $('.amenities h4').text('Amenities: ' + amenitiesList);
-	});
-  
-	// request status
-	$.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
-		// if ok add class available
-	  if (data.status === 'OK') {
-		$('#api_status').addClass('available');
-		// else remove class available
-	  } else {
-		$('#api_status').removeClass('available');
-	  }
-	});
-  });
+        if (this.checked) {
+            selected[id] = name;
+        } else {
+			// else remove from selected
+            delete selected[id];
+        }
+		// join selected
+        const amenitiesList = Object.values(selected).join(', ');
+        $('.amenities h4').text(amenitiesList);
+    });
+
+		// check api status
+    $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+		// if status is OK add class available
+        if (data.status === 'OK') {
+            $('#api_status').addClass('available');
+        } else {
+			// else remove class available
+            $('#api_status').removeClass('available');
+        }
+    });
+});
